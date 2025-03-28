2bb07967a4b59f99211e007b0cad98f873ba7525
AjayRajNegi <ajayrajnegi111@gmail.com>
2025-03-28 14:19:31 +0530

    BouncyCardFeature, optimized for mac.

--- src/app/components/BouncyCardsFeatures.tsx
+++ src/app/components/BouncyCardsFeatures.tsx
@@ -1,62 +1,65 @@
 "use client";
 import React, { ReactNode } from "react";
 import { motion } from "framer-motion";
+import { div } from "motion/react-client";
 
 export const BouncyCardsFeatures = () => {
   return (
-    <section className="mx-auto max-w-7xl px-4 md:px-0 md:pt-[80px] py-12 text-slate-800 bg-gray-950">
-      <div className="mb-8 flex flex-col items-start justify-between gap-4 md:flex-row md:items-end md:px-0">
-        <h2 className="max-w-lg text-4xl font-bold md:text-5xl text-slate-300">
-          Grow faster with our
-          <span className="text-slate-400"> all in one solution</span>
-        </h2>
-        <motion.button
-          whileHover={{ scale: 1.05 }}
-          whileTap={{ scale: 0.95 }}
-          className="whitespace-nowrap rounded-lg bg-slate-900 px-4 py-2 font-medium text-slate-400 shadow-xl transition-colors hover:bg-slate-700"
-        >
-          {/* Learn more */}
-        </motion.button>
-      </div>
-      <div className="mb-4 grid grid-cols-12 gap-4">
-        <BounceCard className="col-span-12 md:col-span-4">
-          <CardTitle>Duration</CardTitle>
-          <div className="absolute bottom-0 left-4 right-4 top-32 translate-y-8 rounded-t-2xl bg-gradient-to-br from-violet-400 to-indigo-400 p-4 transition-transform duration-[250ms] group-hover:translate-y-4 group-hover:rotate-[2deg]">
-            <span className="block text-center font-semibold text-indigo-50">
-              Available in both online and offline modes for flexibility.
-            </span>
-          </div>
-        </BounceCard>
-        <BounceCard className="col-span-12 md:col-span-8">
-          <CardTitle>Format</CardTitle>
-          <div className="absolute bottom-0 left-4 right-4 top-32 translate-y-8 rounded-t-2xl bg-gradient-to-br from-amber-400 to-orange-400 p-4 transition-transform duration-[250ms] group-hover:translate-y-4 group-hover:rotate-[2deg]">
-            <span className="block text-center font-semibold text-orange-50">
-              Year-long course with one session conducted every week for each
-              department
-            </span>
-          </div>
-        </BounceCard>
-      </div>
-      <div className="grid grid-cols-12 gap-4">
-        <BounceCard className="col-span-12 md:col-span-8">
-          <CardTitle>Tools Required</CardTitle>
-          <div className="absolute bottom-0 left-4 right-4 top-32 translate-y-8 rounded-t-2xl bg-gradient-to-br from-green-400 to-emerald-400 p-4 transition-transform duration-[250ms] group-hover:translate-y-4 group-hover:rotate-[2deg]">
-            <span className="block text-center font-semibold text-emerald-50">
-              LinkedIn Premium (optional), Scheduling Tools (Hootsuite, Buffer),
-              Automation Tools (Dux-Soup, Expandi).
-            </span>
-          </div>
-        </BounceCard>
-        <BounceCard className="col-span-12 md:col-span-4">
-          <CardTitle>And finally this</CardTitle>
-          <div className="absolute bottom-0 left-4 right-4 top-32 translate-y-8 rounded-t-2xl bg-gradient-to-br from-pink-400 to-red-400 p-4 transition-transform duration-[250ms] group-hover:translate-y-4 group-hover:rotate-[2deg]">
-            <span className="block text-center font-semibold text-red-50">
-              FEATURE DEMO HERE
-            </span>
-          </div>
-        </BounceCard>
-      </div>
-    </section>
+    <div className="w-full bg-gray-950">
+      <section className="mx-auto max-w-7xl px-4 md:px-0 md:pt-[80px] py-12 text-slate-800 bg-gray-950">
+        <div className="mb-8 flex flex-col items-start justify-between gap-4 md:flex-row md:items-end md:px-0">
+          <h2 className="max-w-lg text-4xl font-bold md:text-5xl text-slate-300">
+            Grow faster with our
+            <span className="text-slate-400"> all in one solution</span>
+          </h2>
+          <motion.button
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+            className="whitespace-nowrap rounded-lg bg-slate-900 px-4 py-2 font-medium text-slate-400 shadow-xl transition-colors hover:bg-slate-700"
+          >
+            {/* Learn more */}
+          </motion.button>
+        </div>
+        <div className="mb-4 grid grid-cols-12 gap-4">
+          <BounceCard className="col-span-12 md:col-span-4">
+            <CardTitle>Duration</CardTitle>
+            <div className="absolute bottom-0 left-4 right-4 top-32 translate-y-8 rounded-t-2xl bg-gradient-to-br from-violet-400 to-indigo-400 p-4 transition-transform duration-[250ms] group-hover:translate-y-4 group-hover:rotate-[2deg]">
+              <span className="block text-center font-semibold text-indigo-50">
+                Available in both online and offline modes for flexibility.
+              </span>
+            </div>
+          </BounceCard>
+          <BounceCard className="col-span-12 md:col-span-8">
+            <CardTitle>Format</CardTitle>
+            <div className="absolute bottom-0 left-4 right-4 top-32 translate-y-8 rounded-t-2xl bg-gradient-to-br from-amber-400 to-orange-400 p-4 transition-transform duration-[250ms] group-hover:translate-y-4 group-hover:rotate-[2deg]">
+              <span className="block text-center font-semibold text-orange-50">
+                Year-long course with one session conducted every week for each
+                department
+              </span>
+            </div>
+          </BounceCard>
+        </div>
+        <div className="grid grid-cols-12 gap-4">
+          <BounceCard className="col-span-12 md:col-span-8">
+            <CardTitle>Tools Required</CardTitle>
+            <div className="absolute bottom-0 left-4 right-4 top-32 translate-y-8 rounded-t-2xl bg-gradient-to-br from-green-400 to-emerald-400 p-4 transition-transform duration-[250ms] group-hover:translate-y-4 group-hover:rotate-[2deg]">
+              <span className="block text-center font-semibold text-emerald-50">
+                LinkedIn Premium (optional), Scheduling Tools (Hootsuite,
+                Buffer), Automation Tools (Dux-Soup, Expandi).
+              </span>
+            </div>
+          </BounceCard>
+          <BounceCard className="col-span-12 md:col-span-4">
+            <CardTitle>And finally this</CardTitle>
+            <div className="absolute bottom-0 left-4 right-4 top-32 translate-y-8 rounded-t-2xl bg-gradient-to-br from-pink-400 to-red-400 p-4 transition-transform duration-[250ms] group-hover:translate-y-4 group-hover:rotate-[2deg]">
+              <span className="block text-center font-semibold text-red-50">
+                FEATURE DEMO HERE
+              </span>
+            </div>
+          </BounceCard>
+        </div>
+      </section>
+    </div>
   );
 };
 
