f6777c25bf95ccafc35a2dd9dad42b32148b8601
AjayRajNegi <ajayrajnegi111@gmail.com>
2025-04-07 13:17:34 +0530

    Last Section

--- src/app/portfolio/DriveSection.tsx
+++ src/app/portfolio/DriveSection.tsx
@@ -8,7 +8,7 @@ export default function DriveSection() {
       </p>
       <p className="mx-auto mt-[25px] text-justify indent-[50px] text-xl tracking-tight md:mt-[50px] md:indent-[200px] md:text-5xl">
         Videos are no longer just about generating impressions. We believe in
-        creating meaningful connections with audiences. Whether it's a
+        creating meaningful connections with audiences. Whether it&apos;s a
         commercial or a YouTube Video at RedSan, we aim to inspire people to
         connect and experience fresh perspectives through videos that last.
       </p>
