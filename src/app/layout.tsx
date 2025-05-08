038159d8895f268fdea1e2061134c42c55e92e08
AjayRajNegi <ajayrajnegi111@gmail.com>
2025-05-08 20:09:33 +0530

    id="faq-schema"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -109,6 +109,7 @@ export default function RootLayout({
       <body className={`${audiowide.className} antialiased`}>
         {children} <Analytics />
         <Script
+          id="faq-schema"
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
